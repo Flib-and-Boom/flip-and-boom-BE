73f2a55db0cdb31d637c29726e7c686154e30a4e
dhannywijaya <dhannywijaya1997@gmail.com>
2023-08-30 17:47:52 +0700

    fix

--- seeders/20230827093256-seeding-user.js
+++ seeders/20230827093256-seeding-user.js
@@ -3,24 +3,24 @@ let user = require("../db/user.json");
 const { hashPassword } = require("../helpers");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    user = user.map((el) => {
-      el.experience = 0;
-      el.balance = 10;
-      el.selectedSkin = "basic";
-      el.selectedChar = "basic";
-      el.mediumScore = 0;
-      el.hardScore = 0;
-      el.impossibleScore = 0;
-      el.createdAt = new Date();
-      el.updatedAt = new Date();
-      el.password = hashPassword(el.password);
-      return el;
-    });
-    await queryInterface.bulkInsert("Users", user);
-  },
+	async up(queryInterface, Sequelize) {
+		user = user.map((el) => {
+			el.experience = 0;
+			el.balance = 10;
+			el.selectedSkin = "default";
+			el.selectedChar = "default";
+			el.mediumScore = 0;
+			el.hardScore = 0;
+			el.impossibleScore = 0;
+			el.createdAt = new Date();
+			el.updatedAt = new Date();
+			el.password = hashPassword(el.password);
+			return el;
+		});
+		await queryInterface.bulkInsert("Users", user);
+	},
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("Users", null);
-  },
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete("Users", null);
+	},
 };
